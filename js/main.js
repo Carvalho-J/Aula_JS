008b04aedc1db495938e108b6a0d52231182a098
Carvalho-J <53278456+Carvalho-J@users.noreply.github.com>
2020-01-21 18:11:31 -0300

    Mais funções estudadas
    Integração do JS e html, manipulação de elementos por meio de botões e algumas funções.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-var nome="Nome Sobrenome";
-alert("JS em arquivo a parte");
-alert("Bem vindo de volta " + "nome");
+var nome = prompt("Digite seu nome");
+alert("Bem vindo de volta, " + nome);
+/*var cidade = prompt("Qual a sua cidade?");
+var idade = prompt("QUAL SUA IDADE?");
+/*alert("JS em arquivo a parte");
+    if idade >= 18){
+        alert("maior de idade");
+    } else 
+        alert("Menor de idade");
+
+function validaIdade(idade){
+    var validar; 
+    if (idade >= 18){
+        validar = True
+    } else{
+        validar = False
+    }
+    return validar;
+}
+    var idade = prompt("Digite Idade: ");
+    console.log(validaIdade(idade));
+*/
+function botaoClicado(){
+    alert("Obrigado por clicar no botão 1. Agora clique no botão 2");
+}
+function botaoClicado2(){
+    document.getElementById("botao2").innerHTML = "Obrigado por clicar no botão 2.";
+}
+function redirecionar(){
+    window.open("https://digitalinnovation.one/");
+}    
+function trocarTexto(){
+    document.getElementById("hover").innerHTML = "Obrigado por seguir instruções de forma tão dedicada"; 
+}
